a12eaafccecf3f0e4c5932781bcf1b0570951702
Thomas Bueno <thomasscolax@gmail.com>
2020-05-05 09:26:08 -0300

    Creating view
    Creating view (HTML,SCSS AND JS),
    configuring helper currency acronyms

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,6 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
+require("./exchanges");
 
 import "../stylesheets/application";
